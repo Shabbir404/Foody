c8f49359b04eeaa0a13de99ac12aeb676eef0f41
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2024-12-18 00:18:22 +0600

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <nav className="bg-gray-800 text-white p-4">
+          <h1>hi</h1>
+        </nav>
+      </div>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
